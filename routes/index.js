f4c1709012a401f3d1e5919dea96ec54b0b333f3
Dvoryan <v.dvoryan@gmail.com>
2018-08-05 16:04:14 +0300

    Fix ConfirmEmail
    Fix hash ConfirmEmail

--- routes/index.js
+++ routes/index.js
@@ -1,123 +1,111 @@
-const express = require('express');
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt  = require('jsonwebtoken');
+const express = require("express");
+const User = require("../models/user");
+const HashForEmail = require("../models/hashForEmail");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const nodemailer = require("nodemailer");
-const expressJwt = require('express-jwt');  
-const authenticate = expressJwt({secret : 'server secret'});
+const expressJwt = require("express-jwt");
+const authenticate = expressJwt({ secret: "server secret" });
 
-let newUser, rand;
-
-const prepareUser = ({_id, nickname, email, isAdmin}) => {
-  return {_id, nickname, email, isAdmin};
-}
+const prepareUser = ({ _id, nickname, email, isAdmin }) => {
+  return { _id, nickname, email, isAdmin };
+};
 /**
  * Function sends an email to confirm the user's email address
  *
- * @param {String} email
+ * @param {Object} user
  */
-function confirmEmail(email) {
+const confirmEmail = user => {
   const smtpTransport = nodemailer.createTransport({
-    service: 'Gmail',
+    service: "Gmail",
     auth: {
-      user: 'together0chat@gmail.com',
-      pass: 'qweasdzxc0',
+      user: "together0chat@gmail.com",
+      pass: "qweasdzxc0"
     }
   });
-  bcrypt.hash(email, 10, (err, hash) => {
+  bcrypt.hash(user.email, 10, (err, hash) => {
     if (err) {
       return next(err);
     }
-    rand = hash;
-    let link = `http://127.0.0.1:8080/verify?hash=${rand}`;
+    let newUser = {
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+      hash
+    };
+    HashForEmail.create(newUser);
+
+    //Delete the cache after 5 minutes of ignoring
+    setTimeout(() => {
+      HashForEmail.findOne({ hash: hash }, (err, hashForEmail) => {
+        hashForEmail.remove();
+      });
+    }, 5 * 60 * 1000);
+
+    let link = `http://127.0.0.1:8080/verify?hash=${newUser.hash}`;
     let mailOptions = {
-      to: email,
-      subject: 'Please confirm your Email account',
-      html: `Hello,<br> Please Click on the link to verify your email.<br><a href=${link}>Click here to verify</a>`,
+      to: user.email,
+      subject: "Please confirm your Email account",
+      html: `Hello,<br> Please Click on the link to verify your email.<br><a href=${link}>Click here to verify</a>`
     };
     smtpTransport.sendMail(mailOptions);
-  })
-}
+  });
+};
 
-router.post('/register',User.validate, async (req, res, next) => {
+router.post("/register", User.validate, async (req, res, next) => {
   if (await User.isUserInDB(req.body.email, req.body.nickname)) {
-    const err = new Error('User exists');
+    const err = new Error("User exists");
     err.status = 422;
     next(err);
   }
-  confirmEmail(req.body.email);
-  newUser = req.body;
-  return res.status(200).json({text: `Email has been sent. Please check the inbox`});
+  confirmEmail(req.body);
+  return res
+    .status(200)
+    .json({ text: `Email has been sent. Please check the inbox` });
 });
 
-router.post('/login', User.authenticate, (req, res) => {
-  req.token = jwt.sign( prepareUser( req.session.user) , 'server secret', { expiresIn: '2h'});
-  res.status(200).json({user: req.session.user, accessToken: req.token});
+router.post("/login", User.authenticate, (req, res) => {
+  req.token = jwt.sign(prepareUser(req.session.user), "server secret", {
+    expiresIn: "2h"
+  });
+  res.status(200).json({ user: req.session.user, accessToken: req.token });
 });
 
-router.get('/validate-token', authenticate, (req, res) => {
+router.get("/validate-token", authenticate, (req, res) => {
   res.status(200).json({ user: req.user });
 });
 
-router.get('/logout', (req, res, next) => {
+router.get("/logout", (req, res, next) => {
   if (req.session) {
-    req.session.destroy((err) => {
+    req.session.destroy(err => {
       if (err) return next(err);
     });
   }
 });
 
-router.get('/verify', async (req, res, next) => {
-  if (req.query.hash === rand) {
+router.get("/verify", (req, res, next) => {
+  HashForEmail.findOne({ hash: `${req.query.hash}` }, (err, hashForEmail) => {
     try {
-      req.session.user = await User.create(newUser);
-      rand = null;
-      return res.status(200).json({
-        user: prepareUser(req.session.user),
-      });
+      let newUser = {
+        nickname: hashForEmail.nickname,
+        email: hashForEmail.email,
+        password: hashForEmail.password
+      };
+      //Add user and delete hash
+      User.create(newUser);
+      hashForEmail.remove();
+      res.redirect("/");
     } catch (err) {
       err.status = 422;
       next(err);
     }
-  }
-  else{
-      let error = new Error('Verification failed');
+    if (err) {
+      let error = new Error("Verification failed");
       error.status = 422;
       next(error);
-  }
+    }
+  });
 });
-
-
-
-
-router.post('/register',User.validate, async (req, res, next) => {
-  if (await User.isUserInDB(req.body.email, req.body.nickname)) {
-    const err = new Error('User exists');
-    err.status = 422;
-    next(err);
-  }
-  confirmEmail(req.body.email);
-  newUser = req.body;
-  return res.status(200).json({text: `Email has been sent. Please check the inbox`});
-});
-
-router.post('/login', User.authenticate, (req, res) => {
-  req.token = jwt.sign( req.session.user.toObject() , 'server secret', { expiresIn: '2h' });
-  res.status(200).json({ user: prepareUser(req.session.user), accessToken: req.token});
-});
-
-router.get('/logout', (req, res, next) => {
-  if (req.session) {
-    req.session.destroy((err) => {
-      if (err) return next(err);
-    });
-  }
-});
-
-router.post('/validate-token', authenticate, (req, res) => {
-  res.status(200).json({ user: prepareUser(req.user) });
-});
-
 
 module.exports = router;
