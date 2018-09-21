5a8941959dd5acb6160adacbad9ae9a70adb1273
Andrew Korin <carbonid1@gmail.com>
2018-09-21 15:59:44 +0300

    15_search router

--- routes/search.js
+++ routes/search.js
@@ -5,7 +5,7 @@ const { User } = require('../models/user');
 
 router.post("/search", async (req, res) => {
   let users = await User.find({}).select("nickname avatar -_id");
-  res.status(200).json({ users });
+  res.status(200).json(users);
 });
 
 module.exports = router;
